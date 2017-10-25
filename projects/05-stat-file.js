e240469ee47c8746d14dc84f7a177d7d53ced1ab
Thorsten Lorenz <thlorenz@gmx.de>
2017-10-25 15:28:50 -0500

    ex: stat file example with two TODOs
    - demonstrates capturing stack traces
    - demonstrates use of --inspect-brk to examine state in DevTools

---
+++ projects/05-stat-file.js
@@ -0,0 +1,49 @@
+const asyncHooks = require('async_hooks')
+const print = require('../lib/print')
+const fs = require('fs')
+
+function captureStack() {
+  var stack = {}
+  Error.captureStackTrace(stack, captureStack)
+  return stack.stack.split('\n').slice(1)
+}
+
+function init(id, type, triggerAsyncId, resource) {
+  // TODO: here we're capturing the callstack during `init`.
+  // Examine the stacktrace and try to find the origin of the `fs.stat` call in our code.
+  // Do the same for the other lifetime events and compare your results.
+  const stack = captureStack()
+  print({ stage: 'init', id, type, triggerAsyncId, stack })
+}
+
+function before(id) {
+  // TODO: uncomment the below debugger statement and run your code with the
+  // `--inspect-brk` flag, or via the `npm run inspect` script.
+  // Then open a Node.js dedicated DevTools (see the Readme for more info).
+  //
+  // Make sure you DON'T have 'Disable async stack traces' checked in the Debugger section of
+  // the DevTools settings.
+  //
+  // Inspect the callstack on the right side of DevTools once you break here.
+  // What can you find out about how Chrome DevTools provides async stacktraces for Node.js?
+
+  // debugger
+  print({ stage: 'before', id })
+}
+
+function after(id) {
+  print({ stage: 'after', id })
+}
+
+function destroy(id) {
+  print({ stage: 'destroy', id })
+}
+
+const hook = asyncHooks.createHook({ init, before, after, destroy })
+hook.enable()
+
+fs.stat(__filename, onfileStated)
+function onfileStated(err, res) {
+  if (err) return print('statError: ' + err.message)
+  print({ stage: 'callback', statResult: res })
+}
